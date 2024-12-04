4a3c97490d9765ef3c9f0c7c6c6c8e47104396ea
rat-baby <mattg5225@gmail.com>
2024-12-04 09:04:35 -0600

    starting part 2

---
+++ solutions/pay1part2.py
@@ -0,0 +1,22 @@
+list1 = []
+list2 = []
+line = []
+counts = []
+
+with open('resources/day1.txt', 'r') as input:
+  lines = input.readlines()
+
+for i, text in enumerate(lines):
+  line = lines[i].split()
+  list1.append(line[0])
+  list2.append(line[1])
+
+list1.sort()
+list2.sort()
+
+for i,value1 in enumerate(list1):
+  count = 0
+  for value2 in list2:
+    if value1 == value2:
+      count = count + 1
+  print(i, count)
